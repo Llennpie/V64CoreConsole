09a990606fb627859cd3966cbc72c8353ecdb02c
Llennpie <parashock64@gmail.com>
2022-08-17 22:09:11 -0400

    Add controller support

--- V64CoreConsole/Program.cs
+++ V64CoreConsole/Program.cs
@@ -14,10 +14,15 @@ namespace V64CoreConsole
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Initializing LibV64Core\n");
+            Console.Title = "V64CoreConsole";
 
             // Initialize LibV64Core
+            Console.WriteLine("Initializing LibV64Core\n");
             Process[] emulatorProcesses = Memory.GetEmulatorProcesses("Project64");
+            if (emulatorProcesses.Length == 0)
+                // Throw exception if Project64 is not open
+                throw new InvalidOperationException("ERROR: Could not find active Project64 process");
+
             Memory.HookEmulatorProcess(emulatorProcesses[0]);
             Console.WriteLine(emulatorProcesses[0].MainWindowTitle);
             Memory.FindBaseAddress();
@@ -34,6 +39,7 @@ namespace V64CoreConsole
             Console.WriteLine("\nWelcome to V64CoreConsole. Type \"help\" for list of commands.\n");
 
             var x = Task.Run(() => Update());
+            var y = Task.Run(() => Controller.Update());
 
             RunCommandLoop();
         }
@@ -42,8 +48,12 @@ namespace V64CoreConsole
         {
             while (command != "")
             {
-                Thread.Sleep(500);
+                Thread.Sleep(50);
                 Core.CoreUpdate();
+
+                // Freeze camera with D-Pad Up + L
+                if (Controller.GetButton(Types.ButtonFlags.U_JPAD | Types.ButtonFlags.L_TRIG))
+                    Core.ToggleFreezeCamera();
             }
 
             return Task.CompletedTask;
@@ -51,12 +61,18 @@ namespace V64CoreConsole
 
         private static void RunCommandLoop()
         {
-            while (command != "")
+            while (command != "quit")
             {
                 Console.Write("> ");
                 command = Console.ReadLine()?.ToLower();
                 switch (command)
                 {
+                    case "":
+                        break;
+
+                    case "quit":
+                        return;
+
                     case "help":
                         Console.WriteLine("\n" +
                             "help - Display this message\n" +
@@ -92,7 +108,7 @@ namespace V64CoreConsole
                         break;
 
                     case "powerswap":
-                        Console.WriteLine("(default, vanish, metal)");
+                        Console.WriteLine("(default, vanish, metal, metalvanish)");
                         Console.Write("Enter one of the above > ");
                         string? chosenPowerUpName = Console.ReadLine();
                         if (chosenPowerUpName != null)
